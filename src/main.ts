2a0769cbf1a736129bae9e4542e0df6cb6ced6a6
wangbin <769460162@qq.com>
2026-01-05 09:53:23 +0800

    删除其他代码

--- src/main.ts
+++ src/main.ts
@@ -42,8 +42,7 @@ import Logger from '@/utils/Logger'
 
 import VueDOMPurifyHTML from 'vue-dompurify-html' // 解决v-html 的安全隐患
 
-// wangEditor 插件注册
-import { setupWangEditorPlugin } from '@/views/bpm/model/form/PrintTemplate'
+
 
 import print from 'vue3-print-nb' // 打印插件
 
@@ -67,8 +66,7 @@ const setupAll = async () => {
   setupAuth(app)
   setupMountedFocus(app)
 
-  // wangEditor 插件注册
-  setupWangEditorPlugin()
+
 
   await router.isReady()
 
